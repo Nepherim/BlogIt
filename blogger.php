80af4479b2edf51977204f949b71d7a4e13b991d
David Gilbert <david@solidgone.com>
2009-01-06 01:52:21 +0000

    new: Declared variable PageType -- might be used in future to define display with alternate templates. Currently only one value: blog, used as hidden on form list. Adding additional page types to array automatically causes list to be used on form. chg: Change FmtPVA routine to handle array as incoming object.

--- blogger.php
+++ blogger.php
@@ -1,5 +1,8 @@
 <?php if (!defined('PmWiki')) exit();
 $RecipeInfo['Blogger']['Version'] = '2009-01-10';
+$blogger['debug']=true;
+debugLog('--------------------');
+#foreach ($_POST as $p=>$k) debugLog($p .'=' .$k, true);
 
 # Common user settable
 SDV($Blogger_DefaultGroup, 'Blog');	#Pre-populates the Pagename field; blogs can exist in *any* group, not simply the default defined here.
@@ -16,15 +19,15 @@ SDV($Blogger_EnablePostDirectives, true); #Set to true to allow posting of direc
 SDV($Blogger_TagSeparator, ', ');
 SDVA($Blogger_StatusType, array('draft'=>'draft', 'publish'=>'publish'));
 SDVA($Blogger_CommentType, array('open'=>'open', 'readonly'=>'read only', 'none'=>'none'));
-SDVA($Blogger_BlogList, array('blog1'=>'blog1'));
+SDVA($Blogger_BlogList, array('blog1'=>'blog1'));  #Ensure 'blog1' key remains; you can rename the blog (2nd parameter). Also define other blogs.
+SDVA($Blogger_PageType, array('blog'=>'blog'));  # INTERNAL USE ONLY
 
 #$FPLTemplatePageFmt
 # Usable on wiki pages
 setFmtPV(array('Now','Blogger_AuthorGroup','Blogger_DefaultGroup','Blogger_CommentGroup','Blogger_CategoryGroup','Blogger_DateEntryFormat',
-	'Blogger_DateDisplayFormat','Blogger_Templates','Blogger_BlogForm','Blogger_CommentForm','Blogger_Type_BLOG'));
-FmtPVA ($Blogger_StatusType, '$Blogger_StatusType');
-FmtPVA ($Blogger_CommentType, '$Blogger_CommentType');
-FmtPVA ($Blogger_BlogList, '$Blogger_BlogList');
+	'Blogger_DateDisplayFormat','Blogger_Templates','Blogger_BlogForm','Blogger_CommentForm'));
+FmtPVA(array('$Blogger_StatusType'=>$Blogger_StatusType, '$Blogger_CommentType'=>$Blogger_CommentType,
+	'$Blogger_BlogList'=>$Blogger_BlogList, '$Blogger_PageType'=>$Blogger_PageType));
 
 # Slow: Set to 1 to exclude listing any pages for which the browser does not currently have read authorization
 $EnablePageListProtect = 0;
@@ -33,14 +36,9 @@ $SearchPatterns['default'][] = '!\\.Group(Print)?(Header|Footer|Attributes)$!';
 $SearchPatterns['default'][] = '!^('. $SiteGroup .'|' .$SiteAdminGroup .'|PmWiki|' .$Blogger_CategoryGroup .')\\.!';
 $SearchPatterns['default'][] = FmtPageName('!^$FullName$!', $pagename);
 
-$blogger['debug']=true;
-debugLog('--------------------');
-#foreach ($_POST as $p=>$k) debugLog($p .'=' .$k, true);
-
 # Internal
 $Blogger_BlogForm = 'blogger-entry';
 $Blogger_CommentForm = 'blogger-comments';
-$Blogger_Type_BLOG = 'blog';
 $Group = PageVar($pagename, '$Group');
 
 addPageStore();
@@ -56,37 +54,35 @@ $AutoCreate['/^' .$Blogger_CategoryGroup .'\./'] = array('ctime' => $Now);
 if ($Group == $Blogger_CategoryGroup)
 	$GroupFooterFmt = '(:include ' .$Blogger_Templates .'#tag-pagelist:)(:nl:)';
 
+# Need to save entrybody in an alternate format (::entrybody:...::), to prevent (:...:) markup confusing the end of the variable definition.
+$PageTextVarPatterns['(::var:...::)'] = '/(\(:: *(\w[-\w]*) *:(?!\))\s?)(.*?)(::\))/s';
 $PmForm[$Blogger_BlogForm] = 'form=' .$Blogger_Templates .'#blog-form fmt=' .$Blogger_Templates .'#blog-post';
 $PmForm[$Blogger_CommentForm] = 'saveto="' .$Blogger_CommentGroup .'/{$Group}-{$Name}-' .date('Ymd\THms') .'" form=' .$Blogger_Templates .'#comment-form fmt=' .$Blogger_Templates .'#comment-post';
 
-# Need to save entrybody in an alternate format (::entrybody:...::), to prevent (:...:) markup confusing the end of the variable definition.
-$PageTextVarPatterns['(::var:...::)'] = '/(\(:: *(\w[-\w]*) *:(?!\))\s?)(.*?)(::\))/s';
-
 debugLog('entryType: '.$entryType. '   action: '.$action. '    Target: '.$_POST['target']);
 # Blog entry being posted from PmForm (new or existing)
-if ($action=='pmform' && $_POST['target']==$Blogger_BlogForm) {
+if ($action && $action=='pmform' && $_POST['target']==$Blogger_BlogForm) {
+	if ($Blogger_EnablePostDirectives == true)
+		$PmFormPostPatterns = array();  # Null out the PostPatterns means that directive markup doesn't get replaced.
+
 	# Change field delimiters from (:...:...:) to (::...:...::) for tags and body; entrybody MUST be the last variable.
 	$ROSPatterns['/\(:entrybody:(.*?)(:\))$$/si'] = '(::entrybody:$1::)';
 	$ROSPatterns['/\(:pmtags:(.*?):\)/si'] = '(::pmtags:$1::)';
-
-	# Null out the PostPatterns means that directive markup doesn't get replaced.
-	if ($Blogger_EnablePostDirectives == true)
-		$PmFormPostPatterns = array();
-
-	saveTags();
 	$_POST['ptv_entrydate'] = strtotime($_POST['ptv_displaydate']); #Store dates in Unix format
-} else {
-	# Prevent (::...:...:) markup from being displayed.
-	# NOTE: Must not be declared pre-processing of pmform blogger-entry, otherwise tags don't get generated.
-	Markup('textvar::', '<split', '/\(::\w[-\w]*:(?!\)).*?::\)/s', '');
-	$entryType = PageVar($pagename,'$:entrytype');
-	if ($entryType == $Blogger_Type_BLOG) {
-		$GroupHeaderFmt = '(:include ' .$Blogger_Templates .'#single-entry-view:)';  #Required for action=browse AND comments when redirected on error.
-		if ($action=='bloggeredit' || ($action=='pmform' && $_POST['target']==$Blogger_BlogForm)){
-			$GroupHeaderFmt = '(:include ' .$Blogger_Templates .'#blog-edit:)';
-		}elseif ($action=='pmform' && $_POST['target']==$Blogger_CommentForm){
-			$DefaultPasswords['edit']='';  #Remove edit password to allow initial posting of comment.
-		}
+	saveTags();
+}else{
+	# NOTE: Must not be declared if processing a pmform, as tags don't get generated.
+	Markup('textvar::', '<split', '/\(::\w[-\w]*:(?!\)).*?::\)/s', '');  # Prevent (::...:...:) markup from being displayed.
+}
+
+$entryType = PageVar($pagename,'$:entrytype');
+if ($entryType && $entryType == $Blogger_PageType_BLOG) {
+	$GroupHeaderFmt = '(:include ' .$Blogger_Templates .'#single-entry-view:)';  #Required for action=browse AND comments when redirected on error.
+	if ($action=='bloggeredit' || ($action=='pmform' && $_POST['target']==$Blogger_BlogForm)){
+		#Need to include GroupHeader on blog entry errors, when &action=edit is not passed back by PmForms.
+		$GroupHeaderFmt = '(:include ' .$Blogger_Templates .'#blog-edit:)';
+	}elseif ($action=='pmform' && $_POST['target']==$Blogger_CommentForm){
+		$DefaultPasswords['edit']='';  #Remove edit password to allow initial posting of comment.
 	}
 }
 
@@ -97,17 +93,16 @@ Markup('blogger', 'fulltext',	'/\(:blogger ([more,intro,select,multiline]+)\s?(.
 function bloggerMarkupHandler($action, $options, $text){
 	if ($action == 'more') {
 		return (strpos($text, $GLOBALS['Blogger_BodyBreak']) !== false ? preg_replace('/{\$FullName}/', $options, $GLOBALS['Blogger_ReadMore']) : '');
-	} elseif ($action == 'intro') {
+	}elseif ($action == 'intro') {
 		list($found,$null) = explode($GLOBALS['Blogger_BodyBreak'], $text);
 		return $found;
-	} elseif ($action == 'select') {
+	}elseif ($action == 'select') {
 		$i = count($GLOBALS[$text]);
 		foreach ($GLOBALS[$text] as $k => $v)
 			$t .= '(:input '. ($i==1?'hidden':'select') .' ' .$options .' "' .$k .'" "' .$v .'":)';
 		return $t;
-	} elseif ($action == 'multiline') {
+	}elseif ($action == 'multiline')
 		return preg_replace('/\n/', '<br />', $text);
-	}
 }
 $Conditions['blogger_ispage'] = 'bloggerIsPage($condparm)';
 function bloggerIsPage($pn){
@@ -165,18 +160,18 @@ function setFmtPV($a){
 		$GLOBALS['FmtPV']['$'.$k]='$GLOBALS["'.$k.'"]';
 }
 # Creates $FmtPV variables for each element of $a, named $n_VALUE
-function FmtPVA ($a, $n) {
-	foreach ($a as $k=>$v)
-		$GLOBALS['FmtPV'][$n .'_' .strtoupper($k)] = "'" .$v ."'";
+function FmtPVA ($a){
+	foreach ($a as $var=>$vals)
+		foreach ($vals as $k=>$v)
+			$GLOBALS['FmtPV'][$var .'_' .strtoupper($k)] = "'" .$v ."'";
 }
-function addPageStore ($n='wikilib.d') {
+function addPageStore ($n='wikilib.d'){
 	$GLOBALS['PageStorePath'] = dirname(__FILE__) ."/" .$n ."/{\$FullName}";
 	$where = count($GLOBALS['WikiLibDirs']);
 	if ($where>1) $where--;
 	array_splice($GLOBALS['WikiLibDirs'], $where, 0, array(new PageStore($GLOBALS['PageStorePath'])));
 }
-function debugLog ($msg, $out=false) {
-	if ($out || (!$out && $GLOBALS['blogger']['debug']) ) {
+function debugLog ($msg, $out=false){
+	if ($out || (!$out && $GLOBALS['blogger']['debug']) )
 		error_log(date('r'). ' [blogger]: '. $msg);
-	}
 }
