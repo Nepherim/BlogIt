346c0ddb8fbe4f77f10fd2c65a4a413a560479a1
David Gilbert <david@solidgone.com>
2009-01-10 20:23:25 +0000

    chg: Updated code layout to group PmWiki declarations.

--- blogger.php
+++ blogger.php
@@ -6,7 +6,7 @@
     For installation and usages instructions refer to: http://pmwiki.com/Cookbooks/Blogger
 */
 $RecipeInfo['Blogger']['Version'] = '2009-01-10';
-if( $VersionNum<2001950 ) echo "<h3>You are running PmWiki version {$Version}. Blogger needs a newer version of PmWiki. Please update to the latest 2.2.0 beta version</h3>";
+if($VersionNum<2001950) echo "<h3>You are running PmWiki version {$Version}. Blogger needs a newer version of PmWiki. Please update to the latest 2.2.0 beta version</h3>";
 $blogger['debug']=true;
 debugLog('--------------------');
 #foreach ($_POST as $p=>$k) debugLog($p .'=' .$k, true);
@@ -35,7 +35,7 @@ SDV($Blogger_TitleSeparator, '-');
 SDVA($Blogger_StatusType, array('draft'=>'$[draft]', 'publish'=>'$[publish]'));
 SDVA($Blogger_CommentType, array('open'=>'$[open]', 'readonly'=>'$[read only]', 'none'=>'$[none]'));
 SDVA($Blogger_BlogList, array('blog1'=>'blog1'));  #Ensure 'blog1' key remains; you can rename the blog (2nd parameter). Also define other blogs.
-SDVA($Blogger_PageType, array('blog'=>'blog'));  # INTERNAL USE ONLY
+SDVA($Blogger_PageType, array('blog'=>'blog'));  #INTERNAL USE ONLY
 
 # ----------------------------------------
 # - Usable on Wiki Pages
@@ -58,14 +58,16 @@ $oldBrowse=$HandleActions['browse'];  #Store old browse action so we can perform
 # ----------------------------------------
 # Prevent viewing source and diff, primarily for Comments, as this would reveal email.
 $HandleAuth['source'] = $HandleAuth['diff'] = 'edit';
-if ($Blogger_SkinTemplate == $SiteGroup .'/Blogger-SkinTemplate')
-	$HTMLStylesFmt['blogger'] = 'h2 .blogger-edit-link a {font-size: 50%;}';
 SDV($PageListCacheDir, $FarmD.'/work.d/');
 SDV($EnablePageIndex, 1);
 include_once($FarmD.'/cookbook/pmform.php');
 include_once($FarmD.'/scripts/guiedit.php');
 addPageStore();
+if ($Blogger_SkinTemplate == $SiteGroup .'/Blogger-SkinTemplate')
+	$HTMLStylesFmt['blogger'] = 'h2 .blogger-edit-link a {font-size: 50%;}';
 
+# ----------------------------------------
+# - Categories
 # Doesn't pick up categories defined as page variables.
 $LinkCategoryFmt = "<a class='categorylink' rel='tag' href='\$LinkUrl'>\$LinkText</a>";
 $CategoryGroup = $Blogger_CategoryGroup;	# Need to explicity set this.
@@ -73,6 +75,8 @@ $AutoCreate['/^' .$Blogger_CategoryGroup .'\./'] = array('ctime' => $Now);
 if ($Group == $Blogger_CategoryGroup)
 	$GroupFooterFmt = '(:include ' .$Blogger_CoreTemplate .'#tag-pagelist:)(:nl:)';
 
+# ----------------------------------------
+# - SearchPatterns
 # Slow: Set to 1 to exclude listing any pages for which the browser does not currently have read authorization
 $EnablePageListProtect = 0;
 $SearchPatterns['default'][] = '!\\.(All)?Recent(Changes|Uploads|Comments)$!';
@@ -80,10 +84,42 @@ $SearchPatterns['default'][] = '!\\.Group(Print)?(Header|Footer|Attributes)$!';
 $SearchPatterns['default'][] = '!^('. $SiteGroup .'|' .$SiteAdminGroup .'|PmWiki|' .$Blogger_CategoryGroup .')\\.!';
 $SearchPatterns['default'][] = FmtPageName('!^$FullName$!', $pagename);
 
+# ----------------------------------------
+# - PmForms
 # Need to save entrybody in an alternate format (::entrybody:...::), to prevent (:...:) markup confusing the end of the variable definition.
 $PageTextVarPatterns['(::var:...::)'] = '/(\(:: *(\w[-\w]*) *:(?!\))\s?)(.*?)(::\))/s';
 $PmForm[$Blogger_BlogForm] = 'form=' .$Blogger_CoreTemplate .'#blog-form fmt=' .$Blogger_CoreTemplate .'#blog-post';
-$PmForm[$Blogger_CommentForm] = 'saveto="' .$Blogger_CommentGroup .'/{$Group}-{$Name}-' .date('Ymd\THms') .'" form=' .$Blogger_CoreTemplate .'#comment-form fmt=' .$Blogger_CoreTemplate .'#comment-post';
+$PmForm[$Blogger_CommentForm] = 'saveto="' .$Blogger_CommentGroup .'/{$Group}-{$Name}-' .date('Ymd\THms')
+	.'" form=' .$Blogger_CoreTemplate .'#comment-form fmt=' .$Blogger_CoreTemplate .'#comment-post';
+
+# ----------------------------------------
+# - Handle Actions
+$HandleActions['browse']='blogger_HandleBrowse';
+SDV($HandleActions['bloggerapprove'], 'bloggerApproveComment');
+SDV($HandleAuth['bloggerapprove'], 'admin');
+
+# ----------------------------------------
+# - Markup
+# (:blogger [more,intro,list,multiline] options:)text(:bloggerend:)
+Markup('blogger', 'fulltext', '/\(:blogger ([more,intro,list,multiline]+)\s?(.*?):\)(.*?)\(:bloggerend:\)/esi',
+	"bloggerMU_$1(PSS('$2'), PSS('$3'))");
+
+# ----------------------------------------
+# - Conditions
+$Conditions['blogger_ispage'] = 'bloggerIsPage($condparm)';
+$Conditions['blogger_isdate'] = 'bloggerIsDate($condparm)';
+$Conditions['blogger_isemail'] =	'email($condparm)';
+
+# ----------------------------------------
+# - Markup Expressions
+# Parameters: 0:if/noif 1:variable 2:value 3:[&&,||]
+$MarkupExpr['bloggerIfVar'] = '(!preg_match("/\{.*?\}/",$args[2])?(!empty($args[3])?$args[3]." ":"").($args[0]=="if"?"if=\"":"")."equal {=\$:$args[1]} $args[2]".($args[0]=="if"?"\"":"") : "")';
+$MarkupExpr['bloggerStripTags'] = 'implode($GLOBALS["Blogger_TagSeparator"],blogger_StripTags($args[0]))';
+$MarkupExpr['bloggerStripMarkup'] = '(preg_match("/\(:".$args[0]."\s(.*?):\)/i", $args[1],$m)!==false ? $m[1] : $args[1])';
+# if [0] != null then [2] or [0]; if [0] is null then [1].
+$MarkupExpr['ifnull'] = '(!empty($args[0])?empty($args[2])?$args[0]:$args[2]:$args[1])';
+$MarkupExpr['bloggerBlogGroups'] = (empty($GLOBALS['Blogger_BlogGroups']) ? '""' : '"group=\"' .$GLOBALS['Blogger_BlogGroups'] .'\""');
+$MarkupExpr['bloggerBasePage'] = 'bloggerBasePage($args[0])';
 
 # ----------------------------------------
 # - Main Processing
@@ -128,11 +164,31 @@ if ($entryType && $entryType == trim($FmtPV['$Blogger_PageType_BLOG'],'\'')){
 }
 
 # ----------------------------------------
+# - HandleActions Definitions
+# ----------------------------------------
+# If PmForms fails validation, and redirects to a browse, we need to define markup, since it isn't done as part of PmForm handling in Main Processing,
+# as markup (tags) isn't processed if markup is defined.
+function blogger_HandleBrowse($pagename){
+	blogger_AddMarkup();
+	$GLOBALS['HandleActions']['browse']=$GLOBALS['oldBrowse'];
+	HandleDispatch($pagename, 'browse');
+}
+function bloggerApproveComment($src, $auth='admin') {
+	$ap = (isset($GLOBALS['_GET']['pn']) ? $GLOBALS['_GET']['pn'] : '');  #Page to approve
+	if ($ap) $old = RetrieveAuthPage($ap,$auth,0, READPAGE_CURRENT);
+	if($old){
+		$new = $old;
+		$new['csum'] = $new['csum:' .$GLOBALS['Now'] ] = $GLOBALS['ChangeSummary'] = 'Approved comment';
+		$_POST['diffclass']='minor';
+		$new['text'] = preg_replace('/\(:commentapproved:(false):\)/', '(:commentapproved:true:)',$new['text']);
+		PostPage($ap,$old,$new);	#Don't need UpdatePage, as we don't require edit functions to run
+	}
+	Redirect($src);
+}
+
+# ----------------------------------------
 # - Markup Definitions
 # ----------------------------------------
-# (:blogger [more,intro,list,multiline] options:)text(:bloggerend:)
-Markup('blogger', 'fulltext', '/\(:blogger ([more,intro,list,multiline]+)\s?(.*?):\)(.*?)\(:bloggerend:\)/esi',
-	"bloggerMU_$1(PSS('$2'), PSS('$3'))");
 function bloggerMU_more($options, $text){
 	return (strpos($text, $GLOBALS['Blogger_BodyBreak']) !== false ? preg_replace('/{\$FullName}/', $options, $GLOBALS['Blogger_ReadMore']) : '');
 }
@@ -152,64 +208,26 @@ function bloggerMU_multiline($options, $text){
 }
 
 # ----------------------------------------
-# - HandleActions Definitions
-# ----------------------------------------
-# If PmForms fails validation, and redirects to a browse, we need to define markup, since it isn't done as part of PmForm handling in Main Processing,
-# as markup (tags) isn't processed if markup is defined.
-$HandleActions['browse']='blogger_HandleBrowse';
-function blogger_HandleBrowse($pagename){
-	blogger_AddMarkup();
-	$GLOBALS['HandleActions']['browse']=$GLOBALS['oldBrowse'];
-	HandleDispatch($pagename, 'browse');
-}
-SDV($HandleActions['bloggerapprove'], 'bloggerApproveComment');
-SDV($HandleAuth['bloggerapprove'], 'admin');
-function bloggerApproveComment($src, $auth='admin') {
-	$ap = (isset($GLOBALS['_GET']['pn']) ? $GLOBALS['_GET']['pn'] : '');  #Page to approve
-	if ($ap) $old = RetrieveAuthPage($ap,$auth,0, READPAGE_CURRENT);
-	if($old){
-		$new = $old;
-		$new['csum'] = $new['csum:' .$GLOBALS['Now'] ] = $GLOBALS['ChangeSummary'] = 'Approved comment';
-		$_POST['diffclass']='minor';
-		$new['text'] = preg_replace('/\(:commentapproved:(false):\)/', '(:commentapproved:true:)',$new['text']);
-		PostPage($ap,$old,$new);	#Don't need UpdatePage, as we don't require edit functions to run
-	}
-	Redirect($src);
-}
-
-# ----------------------------------------
 # - Condition Definitions
 # ----------------------------------------
-$Conditions['blogger_ispage'] = 'bloggerIsPage($condparm)';
 function bloggerIsPage($pn){
 	$mp = MakePageName($GLOBALS['pagename'], $pn);
 	if (empty($mp)) return true;
 	if ($mp==$GLOBALS['pagename']) return false;
 	return PageExists($mp);
 }
-$Conditions['blogger_isdate'] = 'bloggerIsDate($condparm)';
 function bloggerIsDate($d){
 	return true;
 }
-$Conditions['blogger_isemail'] =	'email($condparm)';
 function email($e){
 	return (bool)preg_match(
 		"/^[-_a-z0-9\'+*$^&%=~!?{}]++(?:\.[-_a-z0-9\'+*$^&%=~!?{}]+)*+@(?:(?![-.])[-a-z0-9.]+(?<![-.])\.[a-z]{2,6}|\d{1,3}(?:\.\d{1,3}){3})(?::\d++)?$/iD"
 		,$e);
 }
 
-
 # ----------------------------------------
 # - Markup Expression Definitions
 # ----------------------------------------
-# Parameters: 0:if/noif 1:variable 2:value 3:[&&,||]
-$MarkupExpr['bloggerIfVar'] = '(!preg_match("/\{.*?\}/",$args[2])?(!empty($args[3])?$args[3]." ":"").($args[0]=="if"?"if=\"":"")."equal {=\$:$args[1]} $args[2]".($args[0]=="if"?"\"":"") : "")';
-$MarkupExpr['bloggerStripTags'] = 'implode($GLOBALS["Blogger_TagSeparator"],blogger_StripTags($args[0]))';
-$MarkupExpr['bloggerStripMarkup'] = '(preg_match("/\(:".$args[0]."\s(.*?):\)/i", $args[1],$m)!==false ? $m[1] : $args[1])';
-# if [0] != null then [2] or [0]; if [0] is null then [1].
-$MarkupExpr['ifnull'] = '(!empty($args[0])?empty($args[2])?$args[0]:$args[2]:$args[1])';
-$MarkupExpr['bloggerBlogGroups'] = (empty($GLOBALS['Blogger_BlogGroups']) ? '""' : '"group=\"' .$GLOBALS['Blogger_BlogGroups'] .'\""');
-$MarkupExpr['bloggerBasePage'] = 'bloggerBasePage($args[0])';
 function bloggerBasePage($pn){
 	return preg_replace('/^' .$GLOBALS['Blogger_CommentGroup'] .'[\/\.](.*?)-(.*?)-\d{8}T\d{6}$/','${1}/${2}',$pn);
 }
