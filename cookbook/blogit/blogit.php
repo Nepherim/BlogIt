bab274b6830531dcf04ae3639e77e00bad636ed8
David Gilbert <david@solidgone.org>
2010-04-24 21:29:34 -0400

    chg: Add json_encode funtion for <PHP 5.2.

--- cookbook/blogit/blogit.php
+++ cookbook/blogit/blogit.php
@@ -706,3 +706,32 @@ global $WikiLibDirs;
 function bi_debugLog ($msg, $out=false){
 	if ($out || (!$out && $GLOBALS['BlogIt']['debug']) )  error_log(date('r'). ' [blogit]: '. $msg);
 }
+if (!function_exists('json_encode')) {  #required in <PHP5.2, ref http://www.mike-griffiths.co.uk/php-json_encode-alternative/
+	function json_encode($a=false) {
+		if (is_null($a)) return 'null';
+		if ($a === false) return 'false';
+		if ($a === true) return 'true';
+		if (is_scalar($a)){
+			if (is_float($a))  return floatval(str_replace(",", ".", strval($a)));  #Always use "." for floats.
+			if (is_string($a)) {
+				static $jsonReplaces = array(array("\\", "/", "\n", "\t", "\r", "\b", "\f", '"'), array('\\\\', '\\/', '\\n', '\\t', '\\r', '\\b', '\\f', '\"'));
+				return '"' . str_replace($jsonReplaces[0], $jsonReplaces[1], $a) . '"';
+			}else  return $a;
+		}
+		$isList = true;
+		for ($i = 0, reset($a); $i < count($a); $i++, next($a)){
+			if (key($a) !== $i){
+				$isList = false;
+				break;
+			}
+		}
+		$result = array();
+		if ($isList){
+			foreach ($a as $v) $result[] = json_encode($v);
+			return '[ ' . join(', ', $result) . ' ]';
+		}else{
+			foreach ($a as $k => $v) $result[] = json_encode($k).': '.json_encode($v);
+			return '{ ' . join(', ', $result) . ' }';
+		}
+	}
+}
