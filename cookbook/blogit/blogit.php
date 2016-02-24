83985631910a0480d36d5c4f964843d8329aff39
David Gilbert <david@solidgone.org>
2016-02-24 10:12:22 -0500

    * chg: Updated script timestamp.

--- cookbook/blogit/blogit.php
+++ cookbook/blogit/blogit.php
@@ -5,7 +5,7 @@
 
     For installation and usage instructions refer to: http://pmwiki.com/wiki/Cookbook/BlogIt
 */
-$RecipeInfo['BlogIt']['Version'] = '20160230';  #1.8.0
+$RecipeInfo['BlogIt']['Version'] = '20160224';  #1.8.0
 if ($VersionNum < 2001950)	Abort("<h3>You are running PmWiki version {$Version}. In order to use BlogIt please update to 2.2.1 or later.</h3>");
 
 # ----------------------------------------
